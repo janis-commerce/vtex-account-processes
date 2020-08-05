882d8b91ebddb662f94c0d53bf09e5ae8ba9684d
Gaston Pereyra <gaston.pereyra@fizzmod.com>
2020-08-05 14:36:31 -0300

    Added Basic

---
+++ tests/vtex-account-processes-test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/* const assert = require('assert');
+
+const sinon = require('sinon');
+
+const {
+	VtexAccountProcesses,
+	VtexAccountProcessesError
+} = require('../lib'); */
+
+describe('VtexAccountProcesses', () => {
+
+	// your tests here...
+
+});
